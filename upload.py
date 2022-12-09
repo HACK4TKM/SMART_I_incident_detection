6dca02c6bc38c1a660ee314438f50851851b0fc5
muhammed-mizaj <mdprgamer@gmail.com>
2022-12-10 01:24:37 +0530

    place,time,date find

--- upload.py
+++ upload.py
@@ -1,13 +1,26 @@
 import json
 from web3 import Web3
 import w3storage
+from datetime import datetime
+import geocoder
+from geopy.geocoders import Nominatim
 
+ 
 infura_url = 'https://polygon-mumbai.g.alchemy.com/v2/4DMdz2O8T580302fMXkvfoIvP2AZ2hea'
 web3 = Web3(Web3.HTTPProvider(infura_url))
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+current_date = now.strftime("%d/%m/%Y")
+g = geocoder.ip('me')
+geoLoc = Nominatim(user_agent="GetLoc")
+locname = geoLoc.reverse(g.latlng)
+
+
 
 isConnected = web3.isConnected()
 print(isConnected)
 blocknumber = web3.eth.blockNumber
-
 balance = web3.eth.getBalance('0x24d4E35630e579b8E5807E469E487bf799A22B90')
+
 
