7f4932447701b894e9b4b4e2924fe269eb183ce7
Asher Mathews <msasher123@gmail.com>
2022-12-10 11:12:00 +0530

    made some tweaks to video capture

---
+++ camera.py
@@ -0,0 +1,63 @@
+import cv2
+from detection import AccidentDetectionModel
+import numpy as np
+import os
+import datetime
+import uuid
+
+model = AccidentDetectionModel("model.json", 'model_weights.h5')
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+accident_times=[]
+
+def startapplication():
+    # video = cv2.VideoCapture('accident1.mp4')
+    video = cv2.VideoCapture("rtsp://192.168.1.6:4747/h264_pcm.sdp",cv2.CAP_FFMPEG)
+    
+    frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+    width = int(video.get(cv2. CAP_PROP_FRAME_WIDTH ))
+    height = int(video.get(cv2. CAP_PROP_FRAME_HEIGHT ))
+    fps = video.get(cv2. CAP_PROP_FPS)
+    frame_no=0
+    is_accident=False
+    accident_frames=[]
+    ouput_video = cv2.VideoWriter('results/{}.mp4'.format(str(uuid.uuid1())),cv2.VideoWriter_fourcc(*'MJPG'), 10,(width,height))
+    while True:
+        frame_no=frame_no+1
+        ret, frame = video.read()
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+
+        roi = cv2.resize(gray_frame, (250, 250))
+        seconds = round(frame_no / fps)
+        video_time = datetime.timedelta(seconds=seconds)
+        # print(video_time)
+        pred, prob = model.predict_accident(roi[np.newaxis, :, :])
+        # print(accident_times)
+        if(pred == "Accident"):
+            prob = (round(prob[0][0]*100, 2))
+            accident_times.append(str(video_time))
+            accident_frames.append(frame)
+            # ouput_video.write(frame)
+            # print(frame)
+            # print("Accident")
+            
+
+
+            cv2.rectangle(frame, (0, 0), (280, 40), (0, 0, 0), -1)
+            cv2.putText(frame, pred+" "+str(prob), (20, 30), font, 1, (255, 255, 0), 2)
+
+        if cv2.waitKey(33) & 0xFF == ord('q'):
+            print(accident_frames)
+            print(len(accident_frames))
+            for i in accident_frames:
+                ouput_video.write(i)
+            ouput_video.release()
+            return
+        cv2.imshow('Video', frame)
+    
+    
+
+if __name__ == '__main__':
+    startapplication()
